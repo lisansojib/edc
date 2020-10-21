ae93d5a7d6ade6966b59c3eeb3fc1aafbe9996be
Akib Reza Khan <33821998+akibrezakhan@users.noreply.github.com>
2020-10-22 00:45:16 +0600

    added participant data route and updated dashboard view

---
+++ Participant/Models/ParticipantViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.Participant.Models
+{
+    public class ParticipantViewModel: BaseViewModel
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Title { get; set; }
+        public bool Active { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
